f362b08b36631b4280797646e857f75d85dde17e
Masato Takatani <takatani.masato@gmail.com>
2026-08-22 19:50:37 -0600

    spaceship drawn

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <raylib.h>
 
+#include "spaceship.h"
+
+#define IMAGE_SPACESHIP "Graphics/spaceship.png"
+
 int main(void)
 {
   Color grey = {29, 29, 27, 255};
@@ -9,13 +13,30 @@ int main(void)
   InitWindow(windowWidth, windowHeight, "C Space Invaders");
   SetTargetFPS(60);
 
+  /* declare Spaceship ship as real */
+  Spaceship ship;
+
+  /* spaceship create with graphic path with IMAGE_SPACESHIP */
+  spaceship_create(&ship, (Vector2){0.0f, 0.0f}, IMAGE_SPACESHIP);
+
+  /* calculate position with loaded image size */
+  ship.position.x = (GetScreenWidth() - ship.image.width) / 2.0f;
+  ship.position.y = GetScreenHeight() - ship.image.height;
+
   while (WindowShouldClose() == false) {
 
     BeginDrawing();
     ClearBackground(grey);
 
+    /* draw spaceship */
+    spaceship_draw(&ship);
+
     EndDrawing();
   }
 
+  /* destroy texture */
+  spaceship_destroy(&ship);
+
   CloseWindow();
+  return 0;
 }
